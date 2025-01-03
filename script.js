f8ec82956d6adf03d73119f8f084164e2573ccfc
Aru <arujimenez98@gmail.com>
2025-01-03 11:36:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
         <li class="hover"><a href="../../index.html">Home</a></li>
         <li><a href="https://backloggery.com/aru_star_">Backloggery</a></li>
         <li><a href="mailto:aru@hoshikawa-aru.com">Contact</a></li>
-        <li><a href="../blog_index.html">Blog</a></li>
+        <li><a href="pages/blog_index.html">Blog</a></li>
         <li class="no-hover"></li>
         <li class="no-hover"><a>Hello, and welcome to my website! It is under construction. Please check back in sometime to see the things I've been up to!</a></li>
         </ul>
